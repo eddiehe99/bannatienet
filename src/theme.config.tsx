929c436952c6f0ca8f06c26f3a115f53c1558b0d
Sebastian Döll <sebastian@katallaxie.me>
2024-01-18 07:29:58 +0000

    docs: update home page

--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -2,7 +2,8 @@ const config = {
   logo: <span>Docs Template</span>,
   project: {
     link: 'https://github.com/zeiss/docs-template'
-  }
+  },
+  docsRepositoryBase: 'https://github.com/zeiss/docs-template/tree/main'
 }
 
 export default config
