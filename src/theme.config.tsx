006ce6890c9adc4fa17afc0881d6486f5f6d6841
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2024-08-09 15:20:37 +0800

    feat: add content

--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -1,9 +1,29 @@
 const config = {
-  logo: <span>Docs Template</span>,
+  logo: <span>BannatieNet</span>,
   project: {
-    link: 'https://github.com/zeiss/docs-template'
+    link: 'https://github.com/eddiehe99/bannatienet'
   },
-  docsRepositoryBase: 'https://github.com/zeiss/docs-template/tree/main'
+  docsRepositoryBase: 'https://github.com/eddiehe99/bannatienet/tree/main',
+  editLink: {
+    component: null
+  },
+  feedback: {
+    content: null
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1
+  },
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://nextra.site" target="_blank">
+          Nextra
+        </a>
+        .
+      </span>
+    )
+  }
 }
 
 export default config
