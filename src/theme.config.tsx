87b7e954838540867be305b6a1c653168baa385a
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2024-08-13 23:50:30 +0800

    feat: add metadata

--- src/theme.config.tsx
+++ src/theme.config.tsx
@@ -23,7 +23,26 @@ const config = {
         .
       </span>
     )
-  }
+  },
+  faviconGlyph: '☕',
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="BannatieNet" />
+      <meta
+        property="og:description"
+        content="播客《半拿铁 | 商业沉浮录》中出现的歇后语和俏皮话的【非官方】“数据集”。"
+      />
+      <meta
+        property="og:url"
+        content="https://eddiehe99.github.io/bannatienet/"
+      />
+      <meta
+        property="og:image"
+        content="https://podlink.imgix.net/9a227780e55c4575eacf2492a0b46bb2/poster.jpeg"
+      />
+    </>
+  )
 }
 
 export default config
