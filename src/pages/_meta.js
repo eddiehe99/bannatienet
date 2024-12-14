084d643c6300d668340af453aeed258178eb386e
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2024-12-14 20:39:38 +0800

    fix: replace _meta.json by _meta.js

---
+++ src/pages/_meta.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  index: '主页',
+  episodes: '正篇',
+  'journey-to-the-west': '西游篇',
+  extra: '番外',
+  about: {
+    title: '关于',
+    type: 'page'
+  }
+}
