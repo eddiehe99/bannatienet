392617fe4197917f9009bd1d4f6e71bc3763ebb1
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2025-06-21 10:50:16 +0800

    feat: add journey-to-the-west extra 8

--- src/pages/journey-to-the-west/extra/_meta.js
+++ src/pages/journey-to-the-west/extra/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'monkey-extra-8': '🐒 番外 8',
     'monkey-extra-6': '🐒 番外 6',
     'monkey-extra-4': '🐒 番外 4',
     'monkey-extra-3': '🐒 番外 3',
