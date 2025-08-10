39df6148fd038039a0b72a45a7a6fda9475e10b4
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2025-08-10 21:36:45 +0800

    feat: add the investiture of the gods No. 06

--- src/pages/the-investiture-of-the-gods/_meta.js
+++ src/pages/the-investiture-of-the-gods/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'yin-yang-04': '☯️ 04',
     'yin-yang-02': '☯️ 02',
     'yin-yang-01': '☯️ 01'
 }
