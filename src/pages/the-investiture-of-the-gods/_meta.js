689c3eaaca802fe4e7a0a7e30822cc1f555aa0ba
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2025-10-02 10:52:21 +0800

    feat: upload the investiture of gods No. 08, 09, 10

--- src/pages/the-investiture-of-the-gods/_meta.js
+++ src/pages/the-investiture-of-the-gods/_meta.js
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'yin-yang-10': '☯️ 10',
+    'yin-yang-09': '☯️ 09',
+    'yin-yang-08': '☯️ 08',
     'yin-yang-06': '☯️ 06',
     'yin-yang-04': '☯️ 04',
     'yin-yang-02': '☯️ 02',
