17b9e4ea0b0c889168c1567eec926ba1d6fb67a2
eddiehe99 <erheng.he@m.scnu.edu.cn>
2025-11-12 19:07:02 +0800

    feat: add the investiture of the gods No. 17

--- src/pages/the-investiture-of-the-gods/_meta.js
+++ src/pages/the-investiture-of-the-gods/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'yin-yang-17': '☯️ 17',
     'yin-yang-16': '☯️ 16',
     'yin-yang-15': '☯️ 15',
     'yin-yang-12': '☯️ 12',
