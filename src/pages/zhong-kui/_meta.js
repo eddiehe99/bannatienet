b4f77109bb88a0cbe01e537babe6af2618bddbdb
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2026-01-20 22:52:12 +0800

    feat: add Evil Dispelling Judge episode No. 02

--- src/pages/zhong-kui/_meta.js
+++ src/pages/zhong-kui/_meta.js
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'evil-dispelling-judge-02': '👻 02',
     'evil-dispelling-judge-01': '👻 01',
 }
