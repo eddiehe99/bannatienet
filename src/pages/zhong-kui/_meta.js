28e5f1f889d9bca0425235c936e655a3b01393fa
eddiehe99 <erheng.he@m.scnu.edu.cn>
2026-01-24 17:20:55 +0800

    feat: add Evil Dispelling Judge episode No. 05

--- src/pages/zhong-kui/_meta.js
+++ src/pages/zhong-kui/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'evil-dispelling-judge-05': '👻 05',
     'evil-dispelling-judge-03': '👻 03',
     'evil-dispelling-judge-02': '👻 02',
     'evil-dispelling-judge-01': '👻 01',
