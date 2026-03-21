0e4e4e05ab936790fec1a9b675998e1e52bc7b63
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2026-03-21 10:34:00 +0800

    feat: add episode No. 193

--- src/content/episodes/_meta.js
+++ src/content/episodes/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'number-193': 'No. 193',
     'number-192': 'No. 192',
     'number-191': 'No. 191',
     'number-186': 'No. 186',
