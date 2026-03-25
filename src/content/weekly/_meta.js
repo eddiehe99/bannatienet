725b8e5729b4bed1edd1a26758eaecdaabf80ffc
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2026-03-25 19:49:25 +0800

    feat: add weekly news No. 33

--- src/content/weekly/_meta.js
+++ src/content/weekly/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'number-33': 'No. 33',
     'number-23': 'No. 23',
     'number-13': 'No. 13',
 }
