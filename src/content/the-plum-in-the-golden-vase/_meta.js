7d22e4beb7d76c598c41794783e9b41ad13e5e38
eddiehe99 <erheng.he@m.scnu.edu.cn>
2026-08-09 16:38:52 +0800

    feat: add chapter 7 content for '金瓶梅·绿色版' and update metadata

--- src/content/the-plum-in-the-golden-vase/_meta.js
+++ src/content/the-plum-in-the-golden-vase/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'chin-ping-mei-7': '🪭 7',
     'chin-ping-mei-1': '🪭 5',
     'chin-ping-mei-1': '🪭 1'
 }
