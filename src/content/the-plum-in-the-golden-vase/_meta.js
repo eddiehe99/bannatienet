759bdeebd48929592fb9b6dd066b1af2348645e0
eddiehe99 <erheng.he@m.scnu.edu.cn>
2026-08-25 15:05:59 +0800

    feat: add S2E1 content and metadata for '金瓶梅·绿色版'

--- src/content/the-plum-in-the-golden-vase/_meta.js
+++ src/content/the-plum-in-the-golden-vase/_meta.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
+    'chin-ping-mei-s2-e1': '🪭 金瓶梅·绿色版 S2E1. 老板、员工与员工家属',
     'chin-ping-mei-7': '🪭 金瓶梅·绿色版 7. 年仅二十四，花子虚之死',
     'chin-ping-mei-5': '🪭 金瓶梅·绿色版 5. 刚刚！昔日打虎英雄已刺配！居然跟他嫂子有关？',
     'chin-ping-mei-1': '🪭 金瓶梅·绿色版 1. 清河县高空坠物第一案，受害者竟然有点开心？'
