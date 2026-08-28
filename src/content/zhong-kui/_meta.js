ab736650b98aaff694b7a3079fcb06d8152251d2
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2026-08-28 22:10:06 +0800

    chore: update zhong-kui meta descriptions for clarity and context

--- src/content/zhong-kui/_meta.js
+++ src/content/zhong-kui/_meta.js
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    'evil-dispelling-judge-05': '👻 05',
-    'evil-dispelling-judge-03': '👻 03',
-    'evil-dispelling-judge-02': '👻 02',
-    'evil-dispelling-judge-01': '👻 01',
+    'evil-dispelling-judge-05': '👻 钟馗篇 5. 世上没有醉死鬼，交通才能更顺遂（完结）',
+    'evil-dispelling-judge-03': '👻 钟馗篇 3. 只要狠狠把钱花，不怕咱们不败家',
+    'evil-dispelling-judge-02': '👻 钟馗篇 2. 钟馗要来了，咱们赶紧用银子打个棺材吧！',
+    'evil-dispelling-judge-01': '👻 钟馗篇 1. 我一个状元的底子，怎么就成驱魔大神了？',
 }
