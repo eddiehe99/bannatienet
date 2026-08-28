c686155ab627929ab301889a2d4898cee30e2ffa
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2026-08-28 22:52:18 +0800

    feat: update weekly content metadata for clarity and context

--- src/content/weekly/_meta.js
+++ src/content/weekly/_meta.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    'number-33': 'No. 33',
-    'number-23': 'No. 23',
-    'number-13': 'No. 13',
+    'number-33': 'No. 33 几点睡觉最好，AI 演员正式出道，县域融媒体为谁而造',
+    'number-23': 'No. 23 动物园如何赚钱，南博江南春案，TikTok 靴子落地，杭州立法管胖子',
+    'number-13': 'No. 13 世界 500 强有哪些巨头跌落？立体停车库为何变成“僵尸库”？',
 }
