4522ddd0b442ceea1d01682e4aaaa92bfc3664bf
eddiehe99 <erheng.he@m.scnu.edu.cn>
2026-08-29 23:03:28 +0800

    feat: add descriptive titles to journey-to-the-west metadata and fix heading spacing

--- src/content/journey-to-the-west/extra/_meta.js
+++ src/content/journey-to-the-west/extra/_meta.js
@@ -1,9 +1,9 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    'wukong-extra-8': '🐒 番外 8',
-    'wukong-extra-6': '🐒 番外 6',
-    'wukong-extra-4': '🐒 番外 4',
-    'wukong-extra-3': '🐒 番外 3',
-    'wukong-extra-1': '🐒 番外 1'
+    'wukong-extra-8': '🐒 番外 8. 我套圈套的就是你',
+    'wukong-extra-6': '🐒 番外 6. 牛魔王这回下场就不同了',
+    'wukong-extra-4': '🐒 番外 4. 这面色惨白的和尚，跟沙悟净是何关系？',
+    'wukong-extra-3': '🐒 番外 3. 香火它太旺，净坛我太忙',
+    'wukong-extra-1': '🐒 番外 1. 既然是老大圣的后人，那我就叫孙小圣吧！'
 }
